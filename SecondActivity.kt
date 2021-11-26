d0cc215b6da47b3c765abb62951437a6dca9169b
Anna Sidorova <60873654+AnnaSidorova1@users.noreply.github.com>
2021-11-26 19:58:00 +0300

    Update SecondActivity.kt

--- SecondActivity.kt
+++ SecondActivity.kt
@@ -1,8 +1,8 @@
-package com.example.game_d_anya
+package com.example.version_3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
@@ -10,7 +10,7 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         button.setOnClickListener {
-            Toast.makeText(applicationContext,"CORRECT!",Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext,"CORRECT!", Toast.LENGTH_SHORT).show()
         }
         button2.setOnClickListener {
             Toast.makeText(applicationContext, "WRONG!", Toast.LENGTH_SHORT).show()
